000fb3487d9a2bed796ff08f4e7926b2d49f09b1
Al Tsaplin <al.tsaplin@gmail.com>
2023-09-26 08:46:42 +0300

    [FIX] Renamed images in DB for more appropriate one.

--- js/cart.js
+++ js/cart.js
@@ -1,17 +1,24 @@
-"use strict";
+'use strict';
 
 const cart = function () {
-    const cartBtn = document.querySelector('.button-cart');
-    const cart = document.getElementById('modal-cart');
-    const closeBtn = cart.querySelector('.modal-close');
+  const cartBtn = document.querySelector('.button-cart'),
+    cart = document.getElementById('modal-cart'),
+    closeBtn = cart.querySelector('.modal-close'),
+    goodsContainer = document.querySelector('.long-goods-list');
 
-    cartBtn.addEventListener('click', () => {
-        cart.style.display = 'flex';
-    });
+  cartBtn.addEventListener('click', () => {
+    cart.style.display = 'flex';
+  });
 
-    closeBtn.addEventListener('click', () => {
-        cart.style.display = '';
-    });
-}
+  closeBtn.addEventListener('click', () => {
+    cart.style.display = '';
+  });
+
+  if (goodsContainer) {
+    goodsContainer.addEventListener('click', (evt) => {
+      console.log(evt.target);
+    })
+  }
+};
 
 cart();
